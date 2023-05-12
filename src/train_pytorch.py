e2881e061c04f6adbcbe6b9bd8e75030afa317a1
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2023-05-12 13:43:26 +0100

    csv file added

--- src/train_pytorch.py
+++ src/train_pytorch.py
@@ -1,32 +1,25 @@
-import os
 import argparse
-
-import numpy as np
 import random
-import torch
-import sys
+from pathlib import Path
 
-from torch.utils.data.sampler import SubsetRandomSampler
-from torchvision import models
-from torch import optim, nn
-from torchvision import transforms
-from torch.utils.data import Dataset
-from torch.utils.tensorboard import SummaryWriter
-import time
-import pandas as pd
-from PIL import Image
-from sklearn.metrics import confusion_matrix
 import matplotlib
 import matplotlib.pyplot as plt
-import cv2
-from pathlib import Path
+import numpy as np
+import pandas as pd
+import torch
+from PIL import Image
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from torch import nn, optim
+from torch.utils.data import Dataset
+from torchvision import models, transforms
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", help="pre-trained model", required=True)
 parser.add_argument(
     "--resume", action="store_true", help="resume from most recent checkpoint"
 )
-parser.add_argument("--n_epochs", type=int, default=150, help="number of epochs")
+parser.add_argument("--n_epochs", type=int, default=2, help="number of epochs")
 parser.add_argument("--lr", type=float, default=0.00001, help="learning rate")
 parser.add_argument("--dropout_level", type=float, default=0.4, help="dropout")
 parser.add_argument("--w_decay", type=float, default=0.00001, help="weight decay")
@@ -46,64 +39,8 @@ torch.cuda.manual_seed(opt.seed_n)
 
 num_classes = 4
 
-# setup tensorboard stuff
-writer = SummaryWriter(f"/tmp/tensorboard/{int(time.time())}_{opt.model}")
-
 result_path = f"{Path.home()}/Data/Mouse/Results/"
 pretrain_file = opt.model + "_pretrain_model_state.cpt"
-
-
-def check_valid(path):
-    path = Path(path)
-    return not path.stem.startswith(".")
-
-
-def plot_error_matrix(cm, classes, cmap=plt.cm.Blues):
-    """Plot the error matrix for the neural network models"""
-
-    from sklearn.metrics import confusion_matrix
-    import itertools
-
-    plt.imshow(cm, interpolation="nearest", cmap=cmap)
-    # plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    print(cm)
-
-    thresh = cm.max() / 2.0
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(
-            j,
-            i,
-            cm[i, j],
-            horizontalalignment="center",
-            color="white" if cm[i, j] > thresh else "black",
-        )
-
-    plt.ylabel("Ground Truth")
-    plt.xlabel("Prediction")
-    plt.tight_layout()
-
-
-def CCM(cnf_labels, cnf_predictions):
-    class_names = ["2", "3", "4", "5"]
-    cnf_matrix = confusion_matrix(cnf_labels, cnf_predictions)
-    np.set_printoptions(precision=2)
-
-    # Normalise
-    cnf_matrix = cnf_matrix.astype("float") / cnf_matrix.sum(axis=1)[:, np.newaxis]
-    cnf_matrix = cnf_matrix.round(4)
-    matplotlib.rcParams.update({"font.size": 16})
-
-    # Plot normalized confusion matrix
-    plt.figure()
-    plot_error_matrix(cnf_matrix, classes=class_names)
-    plt.tight_layout()
-    filename = "alexnet_nweight.pdf"
-    plt.savefig(filename, format="PDF", bbox_inches="tight")
-    plt.show()
 
 
 def set_parameter_requires_grad(model):
@@ -142,10 +79,7 @@ def get_model(
         model.classifier[-1] = nn.Linear(num_ftrs, num_classes)
 
     elif model_name == "vgg19":
-        model = models.vgg19(pretrained=True)
-        model.features[0] = nn.Conv2d(
-            1, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2)
-        )
+        model = models.vgg19(weights="VGG19_Weights.IMAGENET1K_V1")
         num_ftrs = model.classifier[6].in_features
         model.classifier[6] = nn.Linear(num_ftrs, num_classes)
 
@@ -241,38 +175,13 @@ def test_class(testloader, model):
     accuracy = 0
     model.eval()
 
-    ntest = 0
-    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
     criterion = nn.CrossEntropyLoss()
-    label_list = []
-    prediction_list = []
 
     with torch.no_grad():
         for valid_inputs, valid_labels in testloader:
             valid_inputs, valid_labels = valid_inputs.to(device), valid_labels.to(
                 device
             )
-
-            images = valid_inputs.cpu()
-            images = images[0].numpy().transpose(1, 2, 0)
-            norm_image = cv2.normalize(
-                images,
-                None,
-                alpha=0,
-                beta=255,
-                norm_type=cv2.NORM_MINMAX,
-                dtype=cv2.CV_32F,
-            )
-
-            norm_image = norm_image.astype(np.uint8)
-            scale_percent = 220  # percent of original size
-            width = int(norm_image.shape[1] * scale_percent / 100)
-            height = int(norm_image.shape[0] * scale_percent / 100)
-            dim = (width, height)
-
-            resized_img = cv2.resize(norm_image, dim, interpolation=cv2.INTER_CUBIC)
-
-            gt = valid_labels.cpu() + 2
 
             logps = model(valid_inputs)
 
@@ -282,24 +191,10 @@ def test_class(testloader, model):
             _, predicted = torch.max(logps.data, 1)
             accuracy += (predicted == valid_labels).sum().item()
 
-            pred = predicted.cpu() + 2
-
-            target_label = valid_labels.cpu()
-            pred_label = predicted.cpu()
-
-            label_list.append(target_label)
-            prediction_list.append(pred_label)
-
-            ntest = ntest + 1
-
-    cnf_labels = np.array(label_list)
-    cnf_predictions = np.array(prediction_list)
-
     test_acc = accuracy / len(testloader)
     print("Test Accuracy: %2.5f" % (accuracy / len(testloader)))
 
-    return test_acc, cnf_labels, cnf_predictions
-
+    return test_acc
 
 def save_model(epoch, model, optimizer, filepath=pretrain_file):
     """Save the model and embeddings"""
@@ -333,8 +228,10 @@ class MouseDataset(Dataset):
         img_path = self.df["img_path"].iloc[idx]
         img_name = self.df["filename"].iloc[idx]
 
-        img_path = f"{Path.home()}{img_path}/{img_name}"
+        img_path = f"{Path.home()}/Data/Mouse/preprocessing/{img_path}/{img_name}"
         img = Image.open(img_path)
+        img = img.convert("L")
+        img = img.convert("RGB")
         img_label = self.df.mouse_score.iloc[idx]
 
         if img_label == 2.5 or img_label == 2.0:
@@ -382,38 +279,18 @@ if __name__ == "__main__":
         ]
     )
 
-    main_path = f"{Path.home()}/Data/Sample/"
+    csv_path = "sample_data/df.csv"
+    csv_pd = pd.read_csv(csv_path)
+    
+    df = csv_pd.sort_values("mouse_score")
+    groups = df.groupby(["mouse_score", "mouse_id"])
 
-    # getting the csv files
-    ext = "csv"
-    count = 1
-    df = []
-    for subdir, dirs, files in os.walk(main_path + "train"):
-        for i in files:
-            if i.endswith(ext):
-                csv_path = subdir + "/" + i
-                csv_pd = pd.read_csv(csv_path)
-                if count == 1:
-                    df = csv_pd
-                elif count > 1:
-                    df = pd.concat([df, csv_pd])
-                count += 1
-    train_df = df
+    group_keys = list(groups.groups.keys())
+    np.random.shuffle(group_keys)
 
-    ext = "csv"
-    count = 1
-    df = []
-    for subdir, dirs, files in os.walk(main_path + "test"):
-        for i in files:
-            if i.endswith(ext):
-                csv_path = subdir + "/" + i
-                csv_pd = pd.read_csv(csv_path)
-                if count == 1:
-                    df = csv_pd
-                elif count > 1:
-                    df = pd.concat([df, csv_pd])
-                count += 1
-    test_df = df
+    train_ids, test_ids = train_test_split(group_keys, test_size=0.2)
+    train_df = pd.concat([groups.get_group(id) for id in train_ids])
+    test_df = pd.concat([groups.get_group(id) for id in test_ids])
 
     train_dataset = MouseDataset(train_df, train_transforms)
     test_dataset = MouseDataset(test_df, test_transforms)
@@ -442,10 +319,6 @@ if __name__ == "__main__":
     save_model(epoch, model, optimizer)
 
     model = torch.load(pretrain_file)
-    # state = torch.load(pretrain_file)
-    # model.load_state_dict(state["state_dict"])
-    # model.load_state_dict(state['state_dict'])
-    # model.eval()
 
-    test_acc, cnf_labels, cnf_predictions = test_class(testloader, model)
-    CCM(cnf_labels, cnf_predictions)
+    test_acc = test_class(testloader, model)
+    
